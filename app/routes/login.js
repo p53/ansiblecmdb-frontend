df78d8eaf7b717d8606f0ef3ad33ad2a9e395fab
pavol.ipoth@gmail.com <pavol.ipoth@gmail.com>
2018-04-29 22:48:09 +0200

    Login screen background and various, loading image and various other UI looking improvements

--- app/routes/login.js
+++ app/routes/login.js
@@ -2,5 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model(params) {
+    },
+    activate () {
+      Ember.$('body').addClass('app-login');
+    },
+
+    deactivate () {
+      Ember.$('body').removeClass('app-login');
     }
 });
