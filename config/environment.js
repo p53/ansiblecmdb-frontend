b958b25363908273e176757c24a25f70c64413da
pavol.ipoth@gmail.com <pavol.ipoth@gmail.com>
2018-05-03 22:17:27 +0200

    Setting proper name app

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
     var ENV = {
-      modulePrefix: 'ember-quickstart',
+      modulePrefix: 'ansiblecmdb-frontend',
       environment: environment,
       rootURL: '/',
       locationType: 'auto',
