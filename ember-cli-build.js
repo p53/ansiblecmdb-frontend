545bf2f48ed7c146e0373f444c18d5ed75a15741
pavol.ipoth@gmail.com <pavol.ipoth@gmail.com>
2018-05-03 21:56:45 +0200

    Replacing bootstrap with bootstrap-sass to be able to override bootstrap styling & better login styling

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -14,6 +14,7 @@ module.exports = function(defaults) {
         includePaths: [
           'node_modules/spinkit/scss',
           'node_modules/spinkit/scss/spinners',
+          'bower_components/bootstrap-sass/assets/stylesheets'
         ]
     }
   });
@@ -30,11 +31,9 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-  app.import('bower_components/bootstrap/dist/js/bootstrap.js');
-  app.import('bower_components/bootstrap/dist/css/bootstrap.css');
-  app.import('bower_components/bootstrap/dist/css/bootstrap-theme.css');
-  app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff', {
-    destDir: 'fonts'
+  app.import('bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js');
+  app.import('bower_components/bootstrap-sass/assets/fonts/bootstrap/glyphicons-halflings-regular.woff', {
+    destDir: '/fonts/bootstrap'
   });
 
   app.import('vendor/holder.js');
